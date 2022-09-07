8aca7c1532d7ebf64e18a08107d6a3084955579d
NReisman <nechamaschwartz720@gmail.com>
2022-09-07 15:43:05 -0400

    HMWK70

---
+++ HTML/69/module2.js
@@ -0,0 +1,13 @@
+const myApp = (function () {
+    'use strict';
+
+
+  function stringCaseInsensitiveEquals(string1, string2) {
+    let lower1 = string1.toLowerCase();
+    let lower2 = string2.toLowerCase();
+
+    return lower1 = lower2;
+  }
+
+})();
+
