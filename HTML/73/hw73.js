bccc87eb1147e33fe17208e425ecbae162846892
NReisman <nechamaschwartz720@gmail.com>
2022-09-14 18:31:02 -0400

    HW73

---
+++ HTML/73/hw73.js
@@ -0,0 +1,18 @@
+(function(){
+    
+ 
+
+    function get(selector) {
+        return document.querySelector(selector);
+      }
+    const messageBox = get('#messageBox');
+    const okbutton =  document.getElementById('okBtn');
+
+    okbutton.addEventListener('click', () => {
+        messageBox.style.display = 'none';
+      });
+    
+
+
+})();
+
