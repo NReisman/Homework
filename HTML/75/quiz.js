19480ecf1d03bfdcdc6368a9360eb71821eab2d3
NReisman <nechamaschwartz720@gmail.com>
2022-09-21 18:21:22 -0400

    hmwk76

--- HTML/75/quiz.js
+++ HTML/75/quiz.js
@@ -1,46 +1,57 @@
 window.app = window.app || {};
 window.app.pairs = (function () {
   'use strict';
+  function myPerson() {
 
-
-let population = [];
-let populationCount = 0;
-
-function Person(id,gender,firstName,lastName){
-    //simplified version
-    this.id = id;
-    this.gender = gender;
-    this.firstName = firstName;
-    this.lastName = lastName;
-}
-
-
-
-
-function makeGender(){
-    let genderNum;
-for (let g = 0 ;g < 2; g++){
-   
-   if(g === 0){
-    genderNum= "male";
-   }
-   else if(g === 1){
-    genderNum= "female";
-   }
-return genderNum;
-
-}
-}
-function createPerson(){
-
-    return new Person(++populationCount,makeGender() );
   }
-for(let i = 0; i < 40; i++){
- 
-  population[populationCount] = createPerson();
-}
+  let maleFirst = ["jack", "joe", "john", "joel", "jim"];
+  let femaleFirst = ["sara", "samantha", "sandy", "shirley", "sasha"];
+  let lasts = ["stein", "gold", "green", "smith", "jones"];
 
-console.log("The population consists of: ", population);
+  //main function
+  function makePpl(num) {
+    let ppl = new Array(num);
+    let men = [];
+    //fill array with myPerson
+    for (let i = 0; i < num; ++i) {
+      ppl[i] = new myPerson();
+    }
 
-  
+    //assign ids for each person
+    for (let i = 0; i < ppl.length; i++) {
+      ppl[i].id = i;
+    }
+
+    //assign last names
+    for (let i = 0; i < ppl.length; i++) {
+      ppl[i].lastName = lasts[Math.floor(Math.random() * lasts.length)];
+    }
+
+    //create random number from 0-40 to use to assign male firsts, assign gender to those indeces
+    let x;
+    while (men.length < 20) {
+      x = Math.floor(Math.random() * 40);
+
+      if (!men.includes(x)) {
+        men.push(x);
+        ppl[x].first = maleFirst[Math.floor(Math.random() * maleFirst.length)];
+        ppl[x].gender = "m";
+      }
+    }
+    
+    //assign female firsts and gender, match the pairs and ids
+    let m = 0;
+    for (let i = 0; i < ppl.length; i++) {
+      if (ppl[i].first === undefined) {
+        ppl[i].first = femaleFirst[Math.floor(Math.random() * femaleFirst.length)];
+        ppl[i].gender = "f";
+
+        ppl[i].pair = men[m++];
+        ppl[(ppl[i].pair)].pair = ppl[i].id;
+
+      }
+    }
+    return ppl;
+  }
+  console.log(makePpl(40));
 }());
---
+++ HTML/76/hw76.js
@@ -0,0 +1,69 @@
+window.app = window.app || {};
+window.app.pcsTools = (function () {
+  'use strict';
+
+  function get(selector) {
+    return document.querySelector(selector);
+  }
+
+  function setCss(elem, property, value) {
+    //console.log('in set with', property, value);
+    elem.style[property] = value;
+  }
+
+  function getCss(elem, property) {
+    //console.log('in get with', property);
+    return elem.style[property];
+  }
+
+  function addEventListener(elem, type, callback) {
+    elem.addEventListener(type, callback);
+  }
+
+  function changeColor(elem,property){
+    let a = Math.floor(Math.random() * 255); 
+    let b = Math.floor(Math.random() * 255); 
+    let c = Math.floor(Math.random() * 255);              
+    elem.style[property] = "rgb(" + a + "," + b + "," + c +")";
+    
+
+}
+
+  return {
+    wrap: function (selector) {
+      const elem = get(selector);
+
+      return {
+        /*setCss: (property, value) => setCss(elem, property, value),
+        getCss: (property) => getCss(elem, property)*/
+        css: function (property, value) {
+          if (arguments.length === 2) {
+            setCss(elem, property, value);
+            return this;
+          } else {
+            return getCss(elem, property);
+          }
+        },
+        click: function(callback)  {
+          addEventListener(elem, 'click', callback);
+          return this;
+        },
+        hide: function() {
+          setCss(elem, 'display', 'none');
+          return this;
+        },
+        show: function(displayValue = 'block') {
+          setCss(elem, 'display', displayValue);
+          return this;
+        },
+        //hmwk
+        colorChange: function(time){
+            changeColor('color');
+            setInterval(changeColor, time);
+            return this;
+        }
+        
+      };
+    }
+  };
+})();
