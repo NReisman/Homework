38b74654b3daa2b1457f8bc7322e4392e7a6f7a3
slubowsky <270589+slubowsky@users.noreply.github.com>
2022-12-29 09:42:20 -0500

    comments on quiz

--- HTML/java quiz/orders.js
+++ HTML/java quiz/orders.js
@@ -1,15 +1,24 @@
 (function () {
+
+  // SL - Why would we need to save the totals out of the items instead of just asking the items for their totals?
+  // and having a shared prevTotals wont work. All orders totals are mixed together. You need this to be per order (or at least clear out between processing orders)
 let prevTotals = [];
+
+// SL - what is this one for? You never do anythign with it
 let prevTotal;
+
 class Item {
     constructor(itemName, price, qty) {
         this.itemName = itemName;
         this._price = price;
         this.qty = qty;
+
+        // SL - ok, but we could always calculate when needed. What if someone changes price or quantity? Better remenmber to update total as well...
         this.total = (this._price * this.qty);
 
     }
 
+    // SL - given your implementation looks like only needed code would be prevTotals.push(this.total) rest is useless.
     saveTotal() {
         prevTotal = this.total;
         prevTotals.push(prevTotal);
@@ -23,6 +32,8 @@ class Order {
         this.cName = cName;
         this.cAddress = cAddress;
         this.arrayName = itemArray || [];
+
+        // SL - this is not a getter, its a function - and its not done as a class member, its done by adding a function to each object in the constructor. Why?
         this.grandTotal =
             function () {
                 let total1 = 0;
@@ -37,6 +48,7 @@ class Order {
 
 function myOrders() {
 
+  // SL - not a problem, but async await is much more popular nowadays then .then
     fetch('orders.json')
         .then(function (response) {
             return response.json();
@@ -50,6 +62,8 @@ function myOrders() {
 
 }
 
+// SL - hard coded to work for exactly 2 orders, where first has 2 items and second has 1. We need any number of orders and any number of items. We should be looping through arrays, not hard coding things
+// SL - the total of the second order includes the total of the first order since you dont clear out
 function appendData(data) {
     var mainContainer = document.getElementById("myData");
     let item1 = new Item(data[0].items[0].item, (data[0].items[0].total / data[0].items[0].quantity), data[0].items[0].quantity);
@@ -63,9 +77,11 @@ function appendData(data) {
         + "<br />" + "<br />" + 'Items:' + "<br />" + 'Item: ' + item1.itemName + "<br />" + "Quantity: " + item1.qty + "<br />" + "Price: " + item1._price
         + "<br />" + "<br />" + 'Item: ' + item2.itemName + "<br />" + "Quantity: " + item2.qty + "<br />" + "Price: " + item2._price;
 
-        
+
     let itemA = new Item(data[1].items[0].item, (data[1].items[0].total / data[1].items[0].quantity), data[1].items[0].quantity);
     let itemArray2 = [itemA];
+
+    // SL - will add this items total to prevTotals which already has totals from previous order. Second orders total will include first orders totals.
     itemA.saveTotal();
     let order2 = new Order(data[1].customer, data[1].address, itemArray2);
     var div2 = document.createElement("div");
@@ -84,3 +100,7 @@ window.onload = function () {
     btn.addEventListener("click", myOrders);
 };
 }());
+
+// SL - looks like maybe you could use some more practice. Im not sure why you felt you need to store item totals out of the items. That seems like maybe you need more practice using objects (and classes). Also hard coding the number of orders and items instead of iterating through the arrays I think shows lack of comfort using arrays. I would practice using arrays (maybe redo some of the early array homeworks we did)
+
+// SL - grade 80
