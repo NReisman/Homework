18fe2c89e7e8dbd9bca26a389e592070f80128ee
NReisman <nechamaschwartz720@gmail.com>
2023-02-05 15:20:06 -0500

    FinalJavascript

--- HTML/blog/index.js
+++ HTML/blog/index.js
@@ -1,121 +1,115 @@
-<!DOCTYPE html>
-<html>
-<body>
+/* global $ */
 
-<div id="blogList"></div>
-<div id="selectedBlog"></div>
-<div id="selectedPost"></div>
+'use strict';
+// JSON data representing the blogs and their posts and comments
+const userLink = "https://jsonplaceholder.typicode.com/users";
+const postsLink = "https://jsonplaceholder.typicode.com/posts";
+const commentsLink = "https://jsonplaceholder.typicode.com/comments";
+let users;
+let postsData;
+let commentsData;
 
-<script>
-  // JSON data representing the blogs and their posts and comments
-  const data = {
-    blogs: [
-      {
-        name: 'Blog 1',
-        website: 'www.blog1.com',
-        company: 'Company 1',
-        posts: [
-          {
-            title: 'Post 1',
-            comments: [
-              'Comment 1',
-              'Comment 2',
-              'Comment 3'
-            ]
-          },
-          {
-            title: 'Post 2',
-            comments: [
-              'Comment 4',
-              'Comment 5',
-              'Comment 6'
-            ]
-          }
-        ]
-      },
-      {
-        name: 'Blog 2',
-        website: 'www.blog2.com',
-        company: 'Company 2',
-        posts: [
-          {
-            title: 'Post 3',
-            comments: [
-              'Comment 7',
-              'Comment 8',
-              'Comment 9'
-            ]
-          },
-          {
-            title: 'Post 4',
-            comments: [
-              'Comment 10',
-              'Comment 11',
-              'Comment 12'
-            ]
-          }
-        ]
-      }
-    ]
-  };
-
-  // Function to display the list of blogs
-  function displayBlogList() {
-    const blogList = document.getElementById('blogList');
-    let list = '';
-    for (let i = 0; i < data.blogs.length; i++) {
-      list += '<p>' + data.blogs[i].name + '</p>';
-    }
-    blogList.innerHTML = list;
-  }
-
-  // Function to display the selected blog and its posts
-  function displaySelectedBlog(blogName) {
-    const selectedBlog = document.getElementById('selectedBlog');
-    let blog = '';
-    for (let i = 0; i < data.blogs.length; i++) {
-      if (data.blogs[i].name === blogName) {
-        blog += '<h1>' + data.blogs[i].name + '</h1>';
-        blog += '<p>Website: ' + data.blogs[i].website + '</p>';
-        blog += '<p>Company: ' + data.blogs[i].company + '</p>';
-        blog += '<h2>Posts:</h2>';
-        for (let j = 0; j < data.blogs[i].posts.length; j++) {
-          blog += '<p>' + data.blogs[i].posts[j].title + '</p>';
-          blog += '<button onclick="displayComments(' + i + ', ' + j + ')">Show Comments</button>';
-        }
-        break;
-      }
-    }
-    selectedBlog.innerHTML = blog;
-  }
-
-  // Function to display the comments for a selected post
-   function displayComments(blogIndex, postIndex) {
-    const selectedPost = document.getElementById('selectedPost');
-    let comments = '';
-    comments += '<h2>Comments:</h2>';
-    for (let i = 0; i < data.blogs[blogIndex].posts[postIndex].comments.length; i++) {
-      comments += '<p>' + data.blogs[blogIndex].posts[postIndex].comments[i] + '</p>';
-    }
-    comments += '<button onclick="hideComments()">Hide Comments</button>';
-    selectedPost.innerHTML = comments;
-  }
-  function hideComments(){
-   const selectedPost = document.getElementById('selectedPost');
-   selectedPost.innerHTML ='';
-  }
-  // Call the displayBlogList function to display the initial list of blogs
-  displayBlogList();
-  
-  // Add event listeners to the blog list items to call the displaySelectedBlog function when clicked
-  const blogListItems = document.querySelectorAll('#blogList p');
-  for (let i = 0; i < blogListItems.length; i++) {
-    blogListItems[i].addEventListener('click', function() {
-      displaySelectedBlog(this.innerHTML);
+fetch(userLink)
+    .then(response => response.json())
+    .then(data => {
+        users = data;
+        return fetch(postsLink);
+    })
+    .then(response => response.json())
+    .then(data => {
+        postsData = data;
+        return fetch(commentsLink);
+    })
+    .then(response => response.json())
+    .then(data => {
+        commentsData = data;
+        // Display the initial list of blogs
+        displayUsers(users);
     });
-  }
-</script>
 
-</body>
-</html>
+function displayUsers(data) {
+
+    const blogList = document.getElementById('blogList');
+
+    let list = '';
+    for (let i = 0; i < data.length; i++) {
+
+        list += '<p>' + '<i class="material-icons">' + "language" + '</i>' + "   " +
+            '<a href=\'' + data[i].website + '\'/>' + data[i].website + '</a>';
+        list += '<p>' + '<i class="material-icons">' + "business_center" + '</i>' + "   " +
+            "Company Name: " + data[i].company.name + '</p > ';
+        list += '<p>' + '<i class="material-icons" > ' + "phone" + '</i >' + "   " + "Phone: " + data[i].phone + '</p>';
+        list += '<button onclick="displaySelectedBlog(\'' + data[i].id + '\')" class="btn btn-outline-primary">' + "Explore" + '</button>';
+        list += '<hr>';
+    }
+
+    blogList.innerHTML = list;
+
+
+}
+
+//executes when user is selected
+//takes userID as arg
+function displaySelectedBlog(blogName) {
+
+    const selectedBlog = document.getElementById('selectedBlog');
+    selectedBlog.innerHTML = ''; // Clear the previous content
+    document.getElementById("selectedBlog").style = "display:visible";
+
+    let blog = '';
+    blog += '<button onclick="backBtn()" id="back" class="back">Back</button>' + '<br>' + '<br>';
+    blog += '<h2>Posts:</h2>';
+    let myNum = Number(blogName);
+    Loop:
+    for (let i = myNum; i < postsData.length; i++) {
+        if (postsData[i].userId !== myNum) continue Loop;
+        blog += '<p>' + postsData[i - 1].title + '</p>';
+        blog += '<button onclick="btnFunction(\'' + i + '\')" id=\'' + i + '\'>' + "Show Comments" + '</button>' + '<br>';
+        // addBtnClass();
+    }
+
+    selectedBlog.innerHTML = blog;
+    document.getElementById("blogList").style = "display:none";
+}
+
+function btnFunction(postIdNum) {
+    let elem = document.getElementById(postIdNum);
+    //$(elem).addClass("btn");
+    let selectedPost = document.getElementById('selectedPost');
+
+
+    if (elem.innerHTML === "Show Comments") {
+        $(elem).html("Hide Comments");
+        selectedPost.innerHTML = ''; // Clear the previous content
+
+        let postComment = '';
+        let myNumCom = Number(postIdNum);
+        postComment += '<h1>' + "Comments:" + '</h1>';
+
+        Loop:
+        for (let i = 0; i < commentsData.length; i++) {
+            if (commentsData[i].postId !== myNumCom) continue Loop;
+
+            postComment += '<p>' + commentsData[i].name + '</p>';
+            selectedPost.innerHTML = postComment;
+
+        }
+    }
+    else if (elem.innerHTML === "Hide Comments") {
+        $(elem).html("Show Comments");
+        selectedPost.innerHTML = ''; // Clear the previous content
+    }
+
+}
+
+
+function backBtn() {
+    document.getElementById("blogList").style = "display:visible";
+    document.getElementById("selectedBlog").style = "display:none";
+    let selectedPost = document.getElementById('selectedPost');
+
+    selectedPost.innerHTML = '';
+
+}
+
 
